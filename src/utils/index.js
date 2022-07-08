47a3b6b0718e768a7053a6553444d969e56db7c9
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-07-08 13:59:10 -0400

    refactor: :art: add more colors for tasks

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,22 @@ export const getRandomColor = (id) => {
     .filter((e) => !isNaN(Number(e)))
     .join("");
 
-  const colors = ["#53BF9D", "#F94C66", "#BD4291", "#FFC54D"];
+  const colors = [
+    "#9c27b0",
+    "#F94C66",
+    "#673ab7",
+    "#FFC54D",
+    "#8bc34a",
+    "#2196f3",
+    "#e91e63",
+    "#00bcd4",
+    "#009688",
+    "#4caf50",
+    "#3f51b5",
+    "#53BF9D",
+    "#BD4291",
+    "#03a9f4",
+  ];
 
   if (idx >= colors.length) {
     return colors[idx % colors.length];
