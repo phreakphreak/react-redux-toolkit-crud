1489e6ddfdec0af02f87fc2e9a0a274fe4213384
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-07-08 14:05:42 -0400

    refactor: :sparkles: add custom hook and improve ui form

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,21 +1,15 @@
-import { useState, useId } from "react";
 import { useDispatch } from "react-redux";
-import { addTask } from "../features/tasks/taskSlice";
 import { useLocation } from "wouter";
+import { useTask } from "../hooks/useTask";
 
-function TaskForm() {
+function TaskForm({ taskId }) {
   const dispatch = useDispatch();
   const [, setLocation] = useLocation();
-
-  const [task, setTask] = useState({
-    id: useId(),
-    title: "",
-    description: "",
-  });
+  const [task, action, setTask] = useTask(taskId);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTask(task));
+    dispatch(action(task));
     setLocation("/");
   };
   const handleChange = (e) => {
@@ -33,7 +27,9 @@ function TaskForm() {
       className="flex flex-col flex-wrap gap-4 p-10 rounded-md w-96 bg-slate-800 "
     >
       <header className="pb-2">
-        <h3 className="text-2xl font-bold">Add New Task</h3>
+        <h3 className="text-2xl font-bold">
+          {taskId ? "Edit Task" : "New Task"}
+        </h3>
       </header>
       <label htmlFor="title" className="font-bold">
         Title
@@ -45,7 +41,9 @@ function TaskForm() {
         name="title"
         value={task.title}
         onChange={handleChange}
+        required
       />
+
       <label htmlFor="description" className="font-bold">
         Description
       </label>
@@ -53,11 +51,12 @@ function TaskForm() {
         className="w-full p-3 text-lg rounded-md resize-none h-44 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 bg-slate-700"
         placeholder="Task description"
         name="description"
+        value={task.description}
         onChange={handleChange}
       />
       <input
         type="submit"
-        value="Save Task"
+        value={taskId ? "Update Task" : "Create Task"}
         className="px-5 py-3 mt-2 text-xl font-normal rounded-md hover:bg-sky-400 bg-sky-500 w-fit "
       />
     </form>
