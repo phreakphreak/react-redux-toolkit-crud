dfcb1290239250b6b2e82e885d9a8bbc4673f1dc
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-07-08 13:51:56 -0400

    refactor: edit path in route

--- src/components/TaskView.jsx
+++ src/components/TaskView.jsx
@@ -5,7 +5,7 @@ function TaskView() {
     <>
       <div className="flex flex-row items-center justify-center w-full gap-8">
         <h1 className="text-4xl font-bold">Task Manager</h1>
-        <Link to="/tasks/add">
+        <Link to="/tasks">
           <button className="flex items-center justify-center h-12 px-4 py-2 text-2xl text-white rounded-md w-fit bg-cyan-600 hover:bg-cyan-500">
             Add Task
           </button>
